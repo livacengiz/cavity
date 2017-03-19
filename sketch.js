57384703a19e97d0ad1ab1ff41ad82245dce40a0
Liva Cengiz <ahmetlivacengiz@gmail.com>
2017-03-19 22:51:05 +0300

    implemented radians effect and bezier shape

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function setup() {
   curveSpan = createSpan('curve experience');
   curveSpan.position(20, 160)
   curveSpan.style('font-family', 'monospace')
-  curveSlider = createSlider(0, 255, random(80))
+  curveSlider = createSlider(-250, 255, random(80))
   curveSlider.position(20,180);
 
   // Opaticy
@@ -34,20 +34,35 @@ function setup() {
   opacitySlider = createSlider(10, 100, 40)
   opacitySlider.position(20,230);
 
-  //Background color
 
+  // Radians
+  radiansSpan = createSpan('radians experience');
+  radiansSpan.position(20, 260)
+  radiansSpan.style('font-family', 'monospace')
+  radiansSlider = createSlider(10, 90, 60, 10)
+  radiansSlider.position(20, 280);
+
+  //Background color
   backgroundColor = createCheckbox('background color: black', false);
   backgroundColor.changed(changeBG);
-  backgroundColor.position(20, 260)
+  backgroundColor.position(20, 310)
   backgroundColor.id("bgLabel")
   backgroundColor.style('font-family', 'monospace')
+
+  drawStyle = createRadio();
+  drawStyle.option('bezier');
+  drawStyle.option('line');
+  drawStyle.position(20, 340)
+  drawStyle.style('width', '60px');
+  drawStyle.id('drawStyle');
 
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 255, 255, 255, 1)
 
-  for (var i = 0; i < 360; i++) {
-    circulation.push(circulation[i] = floor(randomGaussian(0,90)))
+  for (var i = 0; i < 180; i++) {
+    circulation.push(circulation[i] = floor(randomGaussian(0,70)))
   }
+
 }
 
 function draw() {
@@ -56,21 +71,33 @@ function draw() {
   complex = complexSlider.value();
   curve = curveSlider.value();
   alpha = map(opacitySlider.value(), 0,100, 0,1)
-
+  rad = radiansSlider.value();
   translate(width/2, height/2);
   for (var i = 0; i < circulation.length; i++) {
-    rotate(TWO_PI/circulation.length);
-    stroke(curve,143, 190, alpha);
+
+
+    stroke(curve,100, hole+80, alpha);
+    noFill()
     var circ = abs(circulation[i]);
-    line(hole, curve, circ, complex);
+    var style = drawStyle.value();
+    if (style == 'line'){
+      rotate(TWO_PI/circulation.length)
+      line(hole, curve, circ, complex);
+    }else {
+      rotate(radians(rad))
+      bezier(hole, 10, circ, curve, curve, circ, 105, complex);
+    }
   }
 }
+
 function changeBG() {
   var spans = document.getElementsByTagName("SPAN")
   var bg = document.getElementById("bgLabel")
+  var drawStyle = document.getElementById("drawStyle")
   if (this.checked()) {
   bgColor = 0;
   bg.style.color = "white";
+  drawStyle.style.color = "white";
   for (var i = 0; i < spans.length; i++) {
       spans[i].style.color = "white";
     }
@@ -80,4 +107,5 @@ function changeBG() {
       spans[i].style.color = "black";
     }
   bg.style.color = "black";
+  drawStyle.style.color = "black";
 }}
